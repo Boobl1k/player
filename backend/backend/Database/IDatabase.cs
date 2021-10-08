657dc77387fdf9d66a5ebb09e44b7ccddfc6447e
DanilMir <mirgayazov02@gmail.com>
2021-10-08 17:25:58 +0300

    init backend solution

---
+++ backend/backend/Database/IDatabase.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Database
+{
+    //для того чтоб применение функций работы с БД нельзя было использовать на любой экземпляр класса
+    public interface IDatabase  
+    {
+        
+    }
+}
---
+++ backend/backend/Database/User.cs
@@ -0,0 +1,26 @@
+﻿namespace backend.Database
+{
+    // public class User : IDatabasable
+    // {
+    //     public int Id { get; set; /*private set?*/ }
+    //
+    //     public string Nickname
+    //     {
+    //         get => Nickname;
+    //         set
+    //         {
+    //             Nickname = value;
+    //             //this.Update(); //
+    //         }
+    //     }
+    //
+    //     public User(int id, string nickname)
+    //     {
+    //         Id = id;
+    //         Nickname = nickname;
+    //     }
+    // }
+    //На будущее если архитектура будет требовать, а пока использую record
+    
+    public record User(int Id, string Nickname) : IDatabase;
+}
