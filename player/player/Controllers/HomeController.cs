4fbef449f23e91f22aaf87c1680ba081988c139f
DanilMir <mirgayazov02@gmail.com>
2021-11-26 22:13:53 +0300

    add: editProfile page

--- player/player/Controllers/HomeController.cs
+++ player/player/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace player.Controllers
             return View();
         }
 
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
