d55e17520d8d8676fb8a8c9ed7a3fdda0c08bd01
DanilMir <mirgayazov02@gmail.com>
2021-12-06 21:34:08 +0300

    add: About page

--- player/player/Controllers/HomeController.cs
+++ player/player/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace player.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
