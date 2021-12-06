41114755e38d8596a56e9d5c7d9b943bec05aafc
DanilMir <mirgayazov02@gmail.com>
2021-12-06 21:14:36 +0300

    add: Contact controll, page

--- player/player/Controllers/HomeController.cs
+++ player/player/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace player.Controllers
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
