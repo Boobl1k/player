c0f5ced17b9843de9bc57929a79894d484c35fa2
Mansur <ahmatowmansur@gmail.com>
2021-12-17 11:30:08 +0300

    q

--- player/player/Controllers/ProfileController.cs
+++ player/player/Controllers/ProfileController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using player.Attributes;
 using player.DB;
@@ -6,18 +7,25 @@ namespace player.Controllers;
 
 public class ProfileController : Controller
 {
+    private readonly PlayerContext _dataContext;
+
+    public ProfileController(PlayerContext dataContext) =>
+        _dataContext = dataContext;
+
     [Authorize, HttpPost]
-    public IActionResult Edit(User user)
+    public IActionResult Edit([FromForm] User user)
     {
-        var currentUser = (User)HttpContext.Items["User"]!;
-        if (user.Login != string.Empty)
+        var currentUser = (User) HttpContext.Items["User"]!;
+        if (user.Login != string.Empty && user.Login is not null && _dataContext.Users.All(u => u.Login != user.Login))
             currentUser.Login = user.Login;
-        if (user.Name != string.Empty)
+        if (user.Name != string.Empty && user.Name is not null)
             currentUser.Name = user.Name;
-        if (user.Surname != string.Empty)
+        if (user.Surname != string.Empty && user.Surname is not null)
             currentUser.Surname = user.Surname;
-        if (user.PhoneNumber != default)
+        if (user.PhoneNumber != default && _dataContext.Users.All(u => u.PhoneNumber != user.PhoneNumber))
             currentUser.PhoneNumber = user.PhoneNumber;
+        _dataContext.Update(currentUser);
+        _dataContext.SaveChanges();
         return Ok();
     }
 }
---
+++ player/player/Controllers/TracksController.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using player.DB;
+
+namespace player.Controllers;
+
+public class TracksController : Controller
+{
+    private readonly PlayerContext _dataContext;
+
+    public TracksController(PlayerContext dataContext) =>
+        _dataContext = dataContext;
+
+    [HttpGet]
+    public IActionResult Search(string searchingRequest, int page)
+    {
+        var tracks = _dataContext.Tracks
+            .Where(t => t.Author.Contains(searchingRequest) || t.Name.Contains(searchingRequest))
+            .Skip(page * 7)
+            .Take(7);
+        return View(tracks);
+    }
+}
