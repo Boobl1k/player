9a8b83cc6f4bd3f34a0a389cd011720598bf6b95
Mansur <ahmatowmansur@gmail.com>
2021-12-17 18:24:33 +0300

    register regex

--- player/player/Controllers/EnteringController.cs
+++ player/player/Controllers/EnteringController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using player.DB;
 using player.Services;
@@ -27,6 +28,17 @@ public class EnteringController : ControllerBase
     public IActionResult Register([FromForm] User user)
     {
         Console.WriteLine($"new user: {user}");
+
+        if (!Regex.IsMatch(user.Login, @"[a-z0-9._%+-]+\@[a-z0-9.-]+\.[a-z]{2,4}$") ||
+            !Regex.IsMatch(user.Password, @"^(?=.*?[A-Z]).{8,}"))
+        {
+            if (!Regex.IsMatch(user.Login, @"[a-z0-9._%+-]+\@[a-z0-9.-]+\.[a-z]{2,4}$"))
+                Console.WriteLine("bad login");
+            if (!Regex.IsMatch(user.Password, @"^(?=.*?[A-Z]).{8,}"))
+                Console.WriteLine("bad pass");
+            return BadRequest();
+        }
+
         if (user.PhoneNumber is < 70000000000 or > 90000000000)
             throw new Exception("bad phone number");
         Expression<Func<User, bool>> searchingExpression = u => u.Login == user.Login;
