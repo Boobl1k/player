de5ab36f2472c4d3373c9f0602d58006c417ed15
Mansur <ahmatowmansur@gmail.com>
2021-12-17 12:34:55 +0300

    liked tracks

--- player/player/Controllers/TracksController.cs
+++ player/player/Controllers/TracksController.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using player.Attributes;
 using player.DB;
 
 namespace player.Controllers;
@@ -20,6 +21,19 @@ public class TracksController : Controller
             .Where(t => t.Author.Contains(searchingRequest) || t.Name.Contains(searchingRequest))
             .Skip(page * 7)
             .Take(7);
+        return View(tracks);
+    }
+
+    [Authorize, HttpGet]
+    public IActionResult Liked()
+    {
+        var userId = (HttpContext.Items["User"] as User)!.Id;
+        var tracks = _dataContext.Tracks
+            .Where(t =>
+            _dataContext.LikedTracks
+                .Where(lt => lt.UserId == userId)
+                .Select(lt => lt.TrackId)
+                .Contains(t.Id));
         return View(tracks);
     }
 }
