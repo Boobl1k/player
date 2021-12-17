23485e85a38262f2f44b662f82974d17f054718f
Mansur <ahmatowmansur@gmail.com>
2021-12-17 18:51:23 +0300

    user db update

---
+++ player/player/Migrations/20211217155015_userAvatarURL.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace player.Migrations
+{
+    public partial class userAvatarURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarURL",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarURL",
+                table: "Users");
+        }
+    }
+}
